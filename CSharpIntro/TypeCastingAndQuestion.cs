cfd4c7383d3142e8f4c8b6bcc05912950a7fdb94
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-04-10 15:45:34 +0300

    ornekler

---
+++ CSharpIntro/TypeCastingAndQuestion.cs
@@ -0,0 +1,40 @@
+﻿
+////Console.WriteLine("Hello, World!");
+
+////string name = Console.ReadLine();
+////string fullname = name + " Yildiz";
+
+////Console.WriteLine(fullname);
+
+
+
+////console dan aldigi sayinin karesini ekrana yazsin
+//Console.WriteLine("Lutfen bir sayi giriniz");
+
+
+//int sayi = Convert.ToInt32(Console.ReadLine());
+
+//int sonuc = sayi * sayi;
+
+//Console.WriteLine("Sonuc " + sonuc);
+
+
+//// 1) Console dan 2 adet sayı alsın ve bu sayıları bolup sonucu ekrana yazdırsın
+
+//string text = "Lutfen 2 sayi giriniz";
+//Console.WriteLine(text);
+
+//var text1 = Console.ReadLine();
+//var text2 = Console.ReadLine();
+
+//double num1 = Convert.ToDouble(text1);
+//double num2 = Convert.ToDouble(text2);
+
+//var num3 = num1 / num2;
+//Console.WriteLine("Sonuç: " + num3);
+
+// 2) Ekrana bir isim gir. Bunun buyuk harfli halini yazdir. Ornegin cagatay girecek CAGATAY gosterecek.
+
+// 3) Ekrana bir isim gir. Bu ismin toplam kac karakter oldugunu yazdirsin
+
+string name = "Cagatay";
