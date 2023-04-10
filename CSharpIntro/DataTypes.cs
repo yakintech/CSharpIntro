5d4ede57daa79d3de42291ba7808da07a183f857
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-04-10 15:04:16 +0300

    Add project files.

---
+++ CSharpIntro/DataTypes.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+//degiskenler
+//sol taraftan sag tarafa ATAMA islemi yapiyorum. sag tarafin tipi ile sol tarafin tipi ayni OLMALI
+
+//string name = "Cagatay";
+//string fullName = name + " " + "Yildiz";
+
+//int number = -5;
+
+//decimal unitPrice = 5.4M;
+//double tax = 3.1;
+//float taxPrice = 55.3F;
+
+//bool onlineStatus = false;
+
+//char firstChar = 'h';
+
+//var age = 30;
+
+
+//dynamic city = "Izmir";
+//city = 5;
+
+//short shortNumber = 0;
+//long longNumber = 999999999;
+
+//object userName = "Cagatay";
+
---
+++ CSharpIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+//string name = Console.ReadLine();
+//string fullname = name + " Yildiz";
+
+//Console.WriteLine(fullname);
+
+
+
+//console dan aldigi sayinin karesini ekrana yazsin
+Console.WriteLine("Lutfen bir sayi giriniz");
+
+
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+int sonuc = sayi * sayi;
+
+Console.WriteLine("Sonuc " + sonuc);
