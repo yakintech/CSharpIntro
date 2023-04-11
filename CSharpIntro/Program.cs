44720736910439bbff6ecf9e4f52326b487c7ce8
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-04-11 14:42:40 +0300

    sorular

--- CSharpIntro/Program.cs
+++ CSharpIntro/Program.cs
@@ -38,5 +38,34 @@ internal class Program
 
         //Disaridan bir ogrenci notu gir. not 70 den buyukse gecti yazsin degilse kaldi yazsin. not 0 dan kucuk veya 100 den buyuk olamaz
 
+        int not = Convert.ToInt32(Console.ReadLine());
+
+        ////   || veya && and
+
+        if (not >= 70)
+        {
+            Console.WriteLine("GECTI");
+        }
+        else if (not < 70)
+        {
+            Console.WriteLine("KALDI");
+        }
+        else
+        {
+            Console.WriteLine("hayirdir kardes");
+        }
+
+        bool status = 10 > 3;
+
+        //1) Disaridan kullanici bir metin girsin. Eger girilen metindeki karakter sayisi 10 dan fazlaysa metin cok uzun yazsin. eger metin girmediyse kutfen bir metin giriniz yazsin.
+
+        //2) Kullanıcıdan 3 adet sınav notu girmesini isteyin.Girilen sınav notlarının ortalaması 0 ve 50 aralığındaysa ekrana “Kaldınız”, 
+        //50 ve 100 aralığındaysa ekrana “Geçtiniz” yazdırın.
+        //Eğer bu değerler arasında değilse ekrana “Geçersiz Değer” yazdırın.
+
+
+        //3) Bir mağazadan alınan ürünlerde 30 TL ve üzerinde kargo bedava, 30 TL altında 6 lira kargo ücreti alınmaktadır. 
+        //Kullanıcıdan alınan toplam ödemeye göre ödenmesi gereken tutarı gösteren programı yazın
+
     }
 }
