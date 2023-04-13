faf24cec05d9e196b17b63e6572a52f5528c2a2e
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2023-04-13 11:05:11 +0300

    donguler start!

---
+++ CSharpIntro/Donguler.cs
@@ -0,0 +1,60 @@
+﻿
+
+////for ve while
+
+//int sayac = 0;
+//while (sayac < 10)
+//{
+//    sayac = sayac + 1;
+//    Console.WriteLine("Hello");
+//}
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    //Console.WriteLine("Hello-2");
+//}
+
+////1 den 100 e kadar olan sayilari ekrana yazdir
+
+//for (int i = 1; i < 101; i++)
+//{
+//    //Console.WriteLine(i);
+//}
+
+////1 den 100 e kadar olan TEK sayilari ekrana yazdir
+//for (int i = 0; i <= 100; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+////Console uzerinden iki adet sayi aliyorum. Bu aldigim iki sayiyi carpma operatoru KULLANMADAN carpin
+
+//Console.WriteLine("Lutfen birinci sayiyi giriniz");
+//int x = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Lutfen ikinci sayiyi giriniz");
+//int y = Convert.ToInt32(Console.ReadLine());
+
+//int toplam = 0;
+//for (int i = 0; i < y; i++)
+//{
+//    toplam = toplam + x;
+//}
+
+////Console.WriteLine(toplam);
+
+//int toplam2 = 0;
+//for (int i = 0; i < x; i++)
+//{
+//    for (int j = 0; j < y; j++)
+//    {
+//        toplam2++;
+//    }
+//}
+
+//Console.WriteLine("Toplam 2 :" + toplam2);
+
---
+++ CSharpIntro/KararYapilari.cs
@@ -0,0 +1,62 @@
+﻿////Console.WriteLine("Lutfen bır sayı gırınız");
+////int sayi = Convert.ToInt32(Console.ReadLine());
+
+////disaridan girilen sayi 100 den buyukse ekrana sayi cok buyuk yazsin
+
+////if (sayi > 100)
+////    Console.WriteLine("Sayi cok buyuk");
+
+////if (sayi > 100)
+////{
+////    Console.WriteLine("Sayi cok buyuk");
+////}
+
+////Disaridan bir fiyat gir. Eger fiyat 200 den buyukse kdv 18 hesaplasin kucukse 8 hesaplasin ve KDV tutarini ekranda gostersin
+
+//Console.WriteLine("Lutfen fiyat giriniz");
+//decimal fiyat = Convert.ToDecimal(Console.ReadLine());
+
+//decimal kdv = 0;
+//if (fiyat >= 200)
+//{
+//    kdv = fiyat * 0.18M;
+//}
+//else
+//{
+//    kdv = fiyat * 0.08M;
+
+//}
+
+//Console.WriteLine(kdv);
+
+
+////Disaridan bir ogrenci notu gir. not 70 den buyukse gecti yazsin degilse kaldi yazsin. not 0 dan kucuk veya 100 den buyuk olamaz
+
+//int not = Convert.ToInt32(Console.ReadLine());
+
+//////   || veya && and
+
+//if (not >= 70)
+//{
+//    Console.WriteLine("GECTI");
+//}
+//else if (not < 70)
+//{
+//    Console.WriteLine("KALDI");
+//}
+//else
+//{
+//    Console.WriteLine("hayirdir kardes");
+//}
+
+//bool status = 10 > 3;
+
+////1) Disaridan kullanici bir metin girsin. Eger girilen metindeki karakter sayisi 10 dan fazlaysa metin cok uzun yazsin. eger metin girmediyse kutfen bir metin giriniz yazsin.
+
+////2) Kullanıcıdan 3 adet sınav notu girmesini isteyin.Girilen sınav notlarının ortalaması 0 ve 50 aralığındaysa ekrana “Kaldınız”, 
+////50 ve 100 aralığındaysa ekrana “Geçtiniz” yazdırın.
+////Eğer bu değerler arasında değilse ekrana “Geçersiz Değer” yazdırın.
+
+
+////3) Bir mağazadan alınan ürünlerde 30 TL ve üzerinde kargo bedava, 30 TL altında 6 lira kargo ücreti alınmaktadır. 
+////Kullanıcıdan alınan toplam ödemeye göre ödenmesi gereken tutarı gösteren programı yazın
